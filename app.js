660073e03a080e2794a15e042b0da1fab87ce3d1
maxwell <antonymaxwell619@gmail.com>
2025-02-28 18:41:20 +0530

    checking connection

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const cors = require('cors');
 
 const app = express();
 
-connectDB();
 
 app.use(cors({
     origin: process.env.ALLOWED_ORIGINS?.split(',') || '*',
@@ -84,6 +83,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+connectDB();
 
 process.on('SIGTERM', () => {
     console.log('Received SIGTERM. Performing graceful shutdown...');
--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
 // config/connectDB.js
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 let dbInstance;
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGODB_URI, {
-           
+        const connection = await mongoose.connect("mongodb+srv://antonymaxwell619:26qzAeDevtsAyFKR@cluster0.ni23s.mongodb.net/shopvista", {
+            ssl: true,
+            serverSelectionTimeoutMS: 5000,
         });
         dbInstance = connection.connection.db;
         console.log('Connected to MongoDB');
         return dbInstance; // Return the MongoDB instance
     } catch (err) {
-        console.error(err);
+        console.error("MongoDB Connection Error ❌", err);
         process.exit(1); // Exit process with failure
     }
 };
